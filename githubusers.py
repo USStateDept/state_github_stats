5a274a4dc69fea44d40d6d6c0828410f140baa72
nathanhilbert <nathanhilbert@gmail.com>
2015-01-30 16:16:10 -0500

    json output

--- githubusers.py
+++ githubusers.py
@@ -2,6 +2,8 @@ from pygithub3 import Github
 import ConfigParser
 import sys
 import os
+import json
+
 
 config = ConfigParser.ConfigParser()
 config.read(['.githubconfig', os.path.expanduser('~/.privatestuff/.githubconfig')])
@@ -21,6 +23,8 @@ if not username or not password:
 
 gh = Github(login=username, password=password)
 
+totalstats = {"orgrepos":0, "memberrepos":0, "nummembers":0}
+
 ORGNAME = 'USStateDept'
 
 orgrepos_contribs = {}
@@ -33,22 +37,19 @@ for repo in gh.repos.list_by_org(ORGNAME).all():
         con_array.append(contriber.login)
     orgrepos_contribs[repo.name] = con_array
 
+totalstats['orgrepos'] = len(orgrepos_contribs.keys())
 
-print orgrepos_contribs
+
 
 #username:
-#   repos
-#       reponame
-#       state equal
-#   stats
 #       numprivaterepos
 #       contribs to org
-#       contributors
 output = {}
 
 
 for member in gh.orgs.members.list(ORGNAME).all():
-    output[member.login] = {"repos":[], "stats":{"orgcontribs": 0, "privaterepos":0}}
+    totalstats['nummembers'] += 1
+    output[member.login] = {"orgcontribs": 0, "privaterepos":0}
     for memrepo in gh.repos.list(member.login).all():
         #find if they actually contribute to their own repos
         try:
@@ -58,17 +59,26 @@ for member in gh.orgs.members.list(ORGNAME).all():
             tempcontribers = []
         for tc in tempcontribers:
             if (member.login == tc.login):
-                output[member.login]["repos"].append(memrepo.name)
-        output[member.login]["stats"]["privaterepos"] = len(output[member.login]["repos"])
+                output[member.login]["privaterepos"] += 1
+                totalstats['memberrepos'] +=1
+                #output[member.login]["repos"].append(memrepo.name)
+        #output[member.login]["stats"]["privaterepos"] = len(output[member.login]["repos"])
 
     #find where they contribute at state
     for reponame, contriblist in orgrepos_contribs.iteritems():
         if member.login in contriblist:
-            output[member.login]["stats"]['orgcontribs'] += 1
+            output[member.login]['orgcontribs'] += 1
 
-print output
 
-#doing azmiria testee
+print "Looked at ", str(totalstats['nummembers']), "members"
+print "Those members had", str(totalstats['memberrepos']), "repositories"
+print "Our Org has", str(totalstats['orgrepos'])
+with open(os.path.dirname(os.path.realpath(__file__)) + "/output.json", 'wb') as jsonfile:
+    json.dump(output, jsonfile)
+
+
+
+
 
 
 
